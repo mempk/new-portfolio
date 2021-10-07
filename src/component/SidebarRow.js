ac4e4575135d8deb9a60006e649824842381cf4a
Madhav <mpk2053@gmail.com>
2021-10-07 01:43:25 -0500

    fix: hide sidebar after selection

--- src/component/SidebarRow.js
+++ src/component/SidebarRow.js
@@ -3,12 +3,19 @@ import { useLocation } from "react-router-dom";
 
 function SidebarRow(props) {
   const history = useLocation();
+  const hideSidebar = (e) => {
+    if (document.documentElement.clientWidth <= 600) {
+      let sideBar = document.querySelector(".sidebar");
+      sideBar.style.display = "none";
+    }
+  };
 
   return (
     <div
       className={`sidebarRow ${
         props.pathName === history.pathname ? "active" : ""
       }`}
+      onClick={(e) => hideSidebar(e)}
     >
       <props.Icon className="sidebarRow_icon" />
       <span className="sidebarRow_title">{props.name}</span>
