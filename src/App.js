c1623fc69b2e0181e31f99c785c877dcf836823b
Madhav <mpk2053@gmail.com>
2021-10-06 23:44:32 -0500

    fix: add routing for component
    add a profile image
    update favicon and title name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ParticlesBg from "particles-bg";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./css/BodyComponent.css";
+import Sidebar from "./component/Sidebar";
+import AboutMe from "./component/AboutMe";
+import Contact from "./component/Contact";
+import Intro from "./component/Intro";
+import Profile from "./component/Profile";
+import Project from "./component/Project";
+import Work from "./component/Work";
+import MobileNav from "./component/MobileNav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <MobileNav className="mobile_nav" />
+        <div className="App_body">
+          <Sidebar />
+          <div className="body_component">
+            <div className="body_component_main">
+              <Switch>
+                <Route exact path="/" component={AboutMe} />
+                <Route path="/me" component={Intro} />
+                <Route path="/work" component={Work} />
+                <Route path="/project" component={Project} />
+                <Route path="/contact" component={Contact} />
+              </Switch>
+            </div>
+            <Profile />
+          </div>
+        </div>
+      </div>
+      <ParticlesBg color="#e91010" num={100} type="cobweb" bg={true} />
+    </BrowserRouter>
   );
 }
 
